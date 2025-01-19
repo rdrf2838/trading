7e46365c9d3bb757b47de7f6413d4709ba9d03ad
Ivin Lee <rdrf.myfoot@gmail.com>
2025-01-19 14:49:41 +0000

    FgmgCjy1lpK9C

--- trading/divide.py
+++ trading/divide.py
@@ -1,12 +1,12 @@
 def divide_nums(num1, num2):
     """This method will be used to divide num1 by num2
 
-        :param int num1: The first number
-        :param int num2: The second number
+    :param int num1: The first number
+    :param int num2: The second number
 
-        :returns: The answer
+    :returns: The answer
 
-        :rtype: int
+    :rtype: int
     """
     answer = num1 / num2
     return answer
--- trading/multiply.py
+++ trading/multiply.py
@@ -1,12 +1,12 @@
 def multiply_nums(num1, num2):
     """This method will be used to multiply two numbers
 
-        :param int num1: The first number
-        :param int num2: The second number
+    :param int num1: The first number
+    :param int num2: The second number
 
-        :returns: The product of two numbers
+    :returns: The product of two numbers
 
-        :rtype: int
+    :rtype: int
     """
     answer = num1 * num2
     return answer
